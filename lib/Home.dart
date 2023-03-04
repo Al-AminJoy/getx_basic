d25de2e7570d3ea2a23463b3933dab39b8436198
Al-AminJoy <alaminislam3555@gmail.com>
2023-03-04 20:00:51 +0600

    Init

---
+++ lib/Home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hi'),
+          ],
+        ),
+      ),
+    );
+  }
+}
